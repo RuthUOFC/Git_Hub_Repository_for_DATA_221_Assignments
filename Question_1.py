121249cc2bda994d519b355a03983c2a0ed04df5
Ruth_UOFC <Ruthigogo@gmail.com>
2026-01-28 11:41:30 -0700

    This is my solution to the first data science assignment question 1

---
+++ Question_1.py
@@ -0,0 +1,22 @@
+
+threshold = 100
+product = 1
+current_number = 1
+
+
+
+while product < threshold:
+    current_number +=1
+    product = product * current_number
+    Final_product = product
+    print(Final_product)
+
+
+print(f"The integer that caused the product to exceed the threshold is:", current_number)
+
+
+
+
+
+
+
